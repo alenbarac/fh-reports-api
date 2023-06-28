eee11f1f321fd21f75289cfdcd1668b8b84e46be
Alen <info@alenbarac.com>
2023-06-29 01:20:39 +0200

    users table seeder fix

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,27 +15,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $email1 = "cmoretti@commer.com";
+        $email1 = "mail@alenbarac.com";
         $userEmail1 = User::where('email', '=', $email1)->first();
 
-        $email2 = "kevinj@thefishinhole.com";
-        $userEmail2 = User::where('email', '=', $email2)->first();
 
         if ($userEmail1 === null) {
             User::create([
-                'name' => 'Cary',
+                'name' => 'Alen',
                 'email' => $email1,
                 'password' => bcrypt('fh-reports')
             ]);
         } 
 
-        if ($userEmail2 === null) {
-            User::create([
-                'name' => 'Kevin',
-                'email' => $email2,
-                'password' => bcrypt('kevin-fh-reports')
-            ]);
-        }
    
     }
 }
